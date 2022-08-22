ffaf4ec08d543051ccb253108273ea1ebd98b7a7
babs1987 <90322556+babs1987@users.noreply.github.com>
2022-08-22 21:29:33 +0700

    Create 4.Log_to_file.py
    log_to_file script first version

---
+++ 08.21/4.Log_to_file.py
@@ -0,0 +1,24 @@
+from os import path
+from time import sleep
+def logging(func):
+    def wrapper(*args,**kwargs):
+        func(*args,**kwargs)
+        if path.exists("log.txt"):
+            with open("log.txt","a") as fp:
+                fp.write(f"name: {func.__name__} positional args: {args} keyword args: {kwargs} \n")
+        else:
+            with open("log.txt","w") as fp:
+                fp.write(f"name: {func.__name__} positional args: {args} keyword args: {kwargs} \n")
+
+    return wrapper
+
+@logging
+def sleepy(*args,**kwargs):
+   sleep(1)
+
+@logging
+def printy(wordy):
+    print(wordy)
+
+sleepy(d=6)
+printy("romero")
