cc903063e0bb1dc268518e88d6c434e318245576
Gennadiy <renardes@gmail.com>
2022-08-25 08:33:47 +0500

    правки для удобства чтения

--- 08.21/4.Log_to_file.py
+++ 08.21/4.Log_to_file.py
@@ -1,20 +1,22 @@
 from os import path
 from time import sleep
+
+
 def logging(func):
-    def wrapper(*args,**kwargs):
-        func(*args,**kwargs)
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
         if path.exists("log.txt"):
             with open("log.txt","a") as fp:
                 fp.write(f"name: {func.__name__} positional args: {args} keyword args: {kwargs} \n")
         else:
             with open("log.txt","w") as fp:
                 fp.write(f"name: {func.__name__} positional args: {args} keyword args: {kwargs} \n")
-
     return wrapper
 
+
 @logging
-def sleepy(*args,**kwargs):
-   sleep(1)
+def sleepy(*args, **kwargs):
+    sleep(1)
 
 @logging
 def printy(wordy):
