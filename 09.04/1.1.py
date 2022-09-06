4fcb9bed2ff6663f1edbbb89cc143c4e0ffb8a45
babs1987 <90322556+babs1987@users.noreply.github.com>
2022-09-06 11:58:08 +0700

    Create 1.1.py
    Реализуйте шаблон Строитель для формирования текста кода класса с конструктором или без.

---
+++ 09.04/1.1.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+
+class ClassBuilder:
+    def __init__(self, class_name: str):
+        self.cn = class_name
+        self.fields: list[str] = []
+
+    def add_field(self, *field: str | int | bool):
+        """
+        метод для добавления полей в список
+        :param field: пара имя:значение поля
+        :return:
+        """
+        self.fields += [field]
+        return self
+
+    def __str__(self):
+        """
+        формирование готового кода и вывод
+        type_chr: нужен для формирования кавычек у строковых значений
+        :return:
+        """
+        type_chr = lambda t: not isinstance(t, int)
+        if not self.fields:
+            return f"Class {self.cn.title()}:\n\tpass"
+        formatted_fields = '\n\t\t'.join(
+            [f"self.{field[0]} = " + "'" * type_chr(field[1]) + f"{field[1]}" + "'" * type_chr(field[1]) for field in
+             self.fields])
+
+        return f"Class {self.cn.title()}:\n\tdef __init__(self):\n\t\t{formatted_fields}"
+
+
+athlete = ClassBuilder("armwrestler")
+
+print(athlete)
+print()
+athlete.add_field("name", "Alex Kurdecha").add_field("age", 28).add_field("senior", True)
+
+print(athlete)
+
+"""
+Class Armwrestler:
+	pass
+
+Class Armwrestler:
+	def __init__(self):
+		self.name = 'Alex Kurdecha'
+		self.age = 28
+		self.senior = True
+"""
