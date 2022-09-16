af132bffbb81d7821e65fa651c541b1d31cd4b45
babs1987 <90322556+babs1987@users.noreply.github.com>
2022-09-16 11:56:44 +0700

    Create 1.py
    first excercise

---
+++ 09.11/1.py
@@ -0,0 +1,74 @@
+from copy import deepcopy
+
+from constants import *
+
+
+class Note:
+    """Музыкальная нота с возможностью копирования."""
+
+    def __init__(self,
+                 *,
+                 pitch: Pitch,
+                 octave: Octave,
+                 accidental: Accidental = None,
+                 duration: Duration = Duration.QUARTER):
+        self.pitch = pitch
+        self.octave = octave
+        self.accidental = accidental
+        self.duration = duration
+
+    def clone(self, **params):
+        """Создаёт новый экземпляр ноты с теми же параметрами."""
+        res = deepcopy(self)
+        res.__dict__.update(params)
+        # Геннадий что в данном подходе не правильно?
+        # for i, j in params.items():
+        #     print(i,j)
+        #     res.i = j
+        return res
+
+    def __str__(self):
+        return f"Высота:{self.pitch}, Октава:{self.octave}" + f", Accidental: {self.accidental}" * (
+                self.accidental is not None) + f", Продолжительность: {self.duration}"
+
+
+class ScoreNote(Note):
+    """Изображение музыкальной ноты в партитуре."""
+    def __init__(self, *, pitch: Pitch, octave: Octave, stem_up: bool, beam: bool = False,
+                 accidental: Accidental = None,
+                 duration: Duration = Duration.QUARTER):
+        super().__init__(pitch=pitch, octave=octave, accidental=accidental, duration=duration)
+        self.stem_up = stem_up
+        self.beam = beam
+
+
+
+class MIDINote(Note):
+    """Кодирование музыкальной ноты в MIDI протоколе."""
+
+    def __init__(self, *, pitch: Pitch, octave: Octave, velocity: int, accidental: Accidental = None,
+                 duration: Duration = Duration.QUARTER):
+        super().__init__(pitch=pitch, octave=octave, accidental=accidental, duration=duration)
+        self.velocity: int = velocity
+
+    def __str__(self):
+        return super().__str__() + f", скорость: {self.velocity}"
+
+
+note1 = Note(pitch=Pitch.B, octave=Octave.LINE_3, duration=Duration.QUARTER, accidental=Accidental.FLAT)
+
+midi_c3 = MIDINote(pitch=Pitch.C, octave=Octave.LINE_1, velocity=80, duration=Duration.HALF)
+clone_midi = midi_c3.clone(pitch=Pitch.D, duration=Duration.WHOLE)
+
+print("Миди_нота:")
+print(midi_c3,"\n")
+print("Клонированная миди нота с измененными аттрибутами:")
+print(clone_midi)
+
+"""
+Миди_нота:
+Высота:1, Октава:3, Продолжительность: 2, скорость: 80 
+
+Клонированная миди нота с измененными аттрибутами:
+Высота:2, Октава:3, Продолжительность: 1, скорость: 80
+"""
