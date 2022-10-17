8591f15d790ec5802c9c85114572d58702d8623b
babs1987 <90322556+babs1987@users.noreply.github.com>
2022-10-17 10:51:21 +0700

    Документация, аннотации

--- 09.18/1.py
+++ 09.18/1.py
@@ -4,14 +4,20 @@ from random import randint
 
 
 class Generator:
+    """Класс одного статического метода формирующего список случайного числа от 1 до 9"""
+
     @staticmethod
-    def generate(count: int):
+    def generate(count: int)->list:
+        """Формирует список случайного числа от 1 до 9"""
         return [randint(1, 9) for _ in range(count)]
 
 
 class Splitter:
+    """Класс разбивающий квадрат на линии, идентичность которых в дальнейшем необходимо верифицировать"""
+
     @staticmethod
-    def split(array) -> list:
+    def split(array: list[list]) -> list:
+        """Разбивает 2d список на списки """
         result = []
 
         row_count = len(array)
@@ -47,8 +53,11 @@ class Splitter:
 
 
 class Verifier:
+    """Класс верифицирующий квадрат"""
+
     @staticmethod
-    def verify(arrays) -> bool:
+    def verify(arrays: list[list]) -> bool:
+        """Верификация списков (Тру если все списки равны по сумме)"""
         first = sum(arrays[0])
 
         for i in range(1, len(arrays)):
@@ -59,42 +68,48 @@ class Verifier:
 
 
 class MagicSquareGenerator:
+    """Класс формирующий магический квадрат"""
+
     def __init__(self, width: int):
         self.w = width
         self.gen = Generator()
         self.ver = Verifier()
         self.sp = Splitter()
 
-    def generate(self):
+    def generate(self)->str:
+        """Генерирует квадрат с одинаковыми суммами по горизонтали, вертикали, диагонали"""
         while True:
             self.res = [self.gen.generate(self.w) for _ in range(self.w)]
             if self.ver.verify(self.sp.split(self.res)):
-                return "\n".join(map(lambda a: "  ".join(map(str,a)), self.res))
+                return "\n".join(map(lambda a: "  ".join(map(str, a)), self.res))
 
-    def joke_generate(self):
+    def joke_generate(self)->str:
         """
-        генерация "магического" квадрата
-        :return:
+        генерация идеального "магического" квадрата
+        :return: строковое представление квадрата
         """
-        ran = randint(1, 9)
-        return "\n".join(map(lambda a: "  ".join(map(str, a)), [[ran for _ in range(self.w)] for _ in range(self.w)]))
+        ran = randint(1, 999999)
+        return ("\n" * self.w).join(
+            map(lambda a: " ".join(a), [[str(ran) for _ in range(self.w)] for _ in range(self.w)]))
 
 
 a = MagicSquareGenerator(3)
 
 print(a.joke_generate())
 
-
-
 time_ = time.time()
 print(a.generate())
 print(time.time() - time_)
 
 """
-5  5  5
-5  5  5
-5  5  5
-3  9  6
-9  6  3
-6  3  9
+915077 915077 915077
+
+
+915077 915077 915077
+
+
+915077 915077 915077
+7  8  6
+6  7  8
+8  6  7
 """
