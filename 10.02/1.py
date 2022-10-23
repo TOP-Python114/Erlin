f5d1f7e9d4cd578fb190b975d7b61d25ea0ea90f
Gennadiy <renardes@gmail.com>
2022-10-23 11:33:40 +0500

    10.02: комментарии, исправления, итоги

--- 10.02/1.py
+++ 10.02/1.py
@@ -3,8 +3,10 @@ from typing import Optional
 from dataclasses import dataclass
 from enum import Enum
 from string import ascii_lowercase as a_lc
+from shutil import get_terminal_size as gts
 
 import re
+import matrix
 
 
 class SquareColor(int, Enum):
@@ -35,12 +37,13 @@ class Piece:
     color: PieceColor
     kind: PieceKind
     square: Optional['Square']
+    # КОММЕНТАРИЙ: записывая атрибут датакласса здесь, вы тем самым обозначаете, что хотите видеть его среди параметров конструктора — вы хотите? мне представляется, что это не нужно для атрибута индекса с фиксированным значением в момент создания
+    # УДАЛИТЬ: опекуном согласно условию должен быть класс Game
     _current_state: int = 0
-
-    # self._changes: list[Memento] = [Memento(start_balance)]
 
     def __post_init__(self):
         self.removed: bool = False
+        # УДАЛИТЬ: опекуном согласно условию должен быть класс Game
         self._changes: list['Turn'] = [Turn(self, self.square)]
 
     def __del__(self):
@@ -57,9 +60,6 @@ class Piece:
 
     def move(self, end_square: 'Square'):
         """Осуществляет проверку, ход фигуры и взятие фигуры противника."""
-
-        # turn.start = self.square
-        # turn.figure = self
         if end_square.piece is not None:
             if end_square.piece.color is self.color:
                 # попытка взятия своей фигуры
@@ -67,16 +67,16 @@ class Piece:
             else:
                 # взятие фигуры
                 del end_square.piece
+        # УДАЛИТЬ: инициатором согласно условию должен быть класс Game
         state = Turn(self, end_square)
         self.square.piece = None
         self.square = end_square
         end_square.piece = self
 
-        # turn.end = self.square
-
-        # print(end_square)
+        # УДАЛИТЬ: опекуном согласно условию должен быть класс Game
         self._changes += [state]
         self._current_state += 1
+        # УДАЛИТЬ: возвращаемое значение не используется и не аннотировано
         return self._changes[-1]
 
 
@@ -100,14 +100,13 @@ class Chessboard(dict):
 
     class File(dict):
         """Вертикаль игровой доски."""
-
         def __init__(self, file: str, start_color: SquareColor):
             super().__init__()
             for i in range(4):
                 for j in range(2):
-                    rank = i * 2 + j + 1
+                    rank = i*2 + j + 1
                     self[rank] = Square(
-                        list(SquareColor)[start_color - j],
+                        list(SquareColor)[start_color-j],
                         file,
                         str(rank)
                     )
@@ -118,7 +117,7 @@ class Chessboard(dict):
         for i in range(8):
             for _ in range(4):
                 for j in range(2):
-                    self[a_lc[i]] = self.__class__.File(a_lc[i], list(SquareColor)[j - i % 2])
+                    self[a_lc[i]] = self.__class__.File(a_lc[i], list(SquareColor)[j-i%2])
         self.__post_init__()
 
     def __post_init__(self):
@@ -159,13 +158,28 @@ class Chessboard(dict):
         else:
             raise KeyError
 
+    def to_matrix(self):
+        res = ()
+        for i in range(8, 0, -1):
+            res += (tuple(
+                str(sq.piece) if sq.piece else ''
+                for sq in self.__rank(i)
+            ),)
+        return matrix.Matrix(res)
+
 
 class Turn:
-    """Класс хранитель хода(номер хода, фигура, начальное поле, конечное поле"""
+    """Хранит информацию о сделанном ходе."""
     def __init__(self, piece: Optional[Piece], end: Optional[Square], id=None):
+        """
+        :param piece: фигура
+        :param end: конечное поле
+        :param id: номер хода
+        """
         self.id = id
         self.piece = piece
-        if piece:
+        # ИСПОЛЬЗОВАТЬ: проверку, является ли объект None, лучше проводить явно — потому что if piece осуществит приведение к логическому типу (проверку на истинность), а это означает, что вы получите такой же результат, если объект не является None, но возвращает False
+        if piece is not None:
             self.start = piece.square
             self.end = end
         else:
@@ -177,95 +191,106 @@ class Turn:
 
 
 class Game:
-    """Класс игра, способный управлять экземпляром доски, делать ходы, отменять ходы повторять ходы"""
+    """Управляет экземпляром доски, делает ходы, отменяет ходы, повторяет ходы."""
     def __init__(self):
         self.board = Chessboard()
-        self._current_state = 0
-        self._changes = [Turn(None, None)]
+        self._current_turn = 0
+        self.__turns = [Turn(None, None)]
 
+    def __str__(self):
+        return matrix.draw_matrices(self.board.to_matrix(), outer_borders=True)
+
+    # ИСПРАВИТЬ: я бы не стал в этом методе запрашивать в параметры экземпляры Square — по-моему довольно и строковой записи клетки, к доске мы можем обратиться через self.board — а то получается, вы специально ради этого отдельно ссылку на доску сохраняете в b
     def move(self, start: Square, end: Square):
-        """Осуществляет ход"""
-        state = Turn(start.piece, end)
-        state.id = self._current_state + 1
-        self._changes += [state]
-        start.piece.move(end)
-        self._current_state += 1
+        """Проверяет, инициирует и записывает ход фигуры."""
+        # ДОБАВИТЬ: а если на стартовом поле нет фигуры?
+        piece = start.piece
+        turn = Turn(piece, end, self._current_turn+1)
+        # ИСПОЛЬЗОВАТЬ: сначала попытка хода — вдруг некорректный? — потом уже добавление хода в историю
+        piece.move(end)
+        self._current_turn += 1
+        self.__turns += [turn]
 
     @property
     def game_log(self):
-        """Возвращает историю ходов"""
-        return "\n".join(map(str, self._changes[1:]))
+        """Возвращает историю ходов."""
+        return "\n".join(map(str, self.__turns[1:]))
 
     def undo(self):
-        """
-        Откатывает игру на 1 ход
-        """
-        if self._current_state > 0:
-            self.board[str(self._changes[self._current_state].start)].piece = self._changes[
-                self._current_state].end.piece
-            self.board[str(self._changes[self._current_state].end)].piece = None
-            self._current_state -= 1
-            state = self._changes[self._current_state]
-            return state
+        """Откатывает игру на 1 ход."""
+        if self._current_turn > 0:
+            turn = self.__turns[self._current_turn]
+            turn.start.piece = turn.end.piece
+            # ИСПРАВИТЬ: а если последний ход завершился взятием фигуры?
+            turn.end.piece = None
+            self._current_turn -= 1
+            return turn
         return None
 
     def redo(self):
-        """Повторяет отмененный ход"""
-        if self._current_state < len(self._changes) - 1:
-            self._current_state += 1
-            state = self._changes[self._current_state]
+        """Повторяет отмененный ход."""
+        if self._current_turn < len(self.__turns) - 1:
+            self._current_turn += 1
+            turn = self.__turns[self._current_turn]
             # возвращаем фигуру
-            self.board[str(state.end)].piece = self._changes[self._current_state].piece
+            self.board[str(turn.end)].piece = turn.piece
             # обнуляем прошлое поле
-            self.board[str(state.start)].piece = None
-            self.square = state.end
-            return state
+            self.board[str(turn.start)].piece = None
+            # УДАЛИТЬ: что это?
+            self.square = turn.end
+            return turn
         return None
 
     def to_step(self, step: int):
-        """переносит игру к нужному ходу"""
-        if step in [turn.id for turn in self._changes]:
-            if self._current_state < step:
-                while step != self._current_state:
+        """Переносит игру к нужному ходу."""
+        # КОММЕНТАРИЙ: есть ещё вариант переиграть партию до нужного хода — то есть просто поставить все фигуры в начальную позицию и повторить все ходы до нужного =)
+        if step in [turn.id for turn in self.__turns]:
+            if self._current_turn < step:
+                while step != self._current_turn:
                     self.redo()
-                print(f"Игра перемещена в начало {step} хода")
-            elif self._current_state > step:
-                while step != self._current_state+1:
+                # print(f"Игра перемещена в начало {step} хода")
+            elif self._current_turn > step:
+                while step != self._current_turn+1:
                     self.undo()
-                print(f"Игра перемещена в начало {step} хода")
+                # print(f"Игра перемещена в начало {step} хода")
             else:
                 self.undo()
-                print(f"Игра перемещена в начало {step} хода")
+                # print(f"Игра перемещена в начало {step} хода")
         else:
-            print("Выход за рамки ходов")
+            # print("Выход за рамки ходов")
+            pass
 
 
 game = Game()
 b = game.board
 
-# print(game.board["e2"].piece)
-print("Изначальное положение фигур:")
-print(b)
-print("-"*1000)
+print('\nИзначальное положение фигур:')
+# ИСПОЛЬЗОВАТЬ: надо бы, конечно, доску понагляднее как-то выводить
+print(game)
+
 game.move(b["e2"], b["e4"])
 game.move(b["e7"], b["e5"])
 game.move(b["d1"], b["h5"])
 game.move(b["g8"], b["c6"])
 game.move(b["h5"], b["f7"])
-print("\n'Экскурсия ферзя':")
-print(b)
-print("-"*1000)
-game.undo()
-print("\nОтмена хода:")
-print(b)
-game.redo()
-print("\nПовтор хода:")
-print(b)
-print("\nПереход на начало 2 хода:")
-game.to_step(2)
-print(b)
+print('\n"Экскурсия" ферзя:')
+print(game)
 
-print("\nЗапись шахматной партии:")
+game.undo()
+print('\nОтмена хода:')
+# КОММЕНТАРИЙ: вот вам и потерянная пешка, которая не вернулась при отмене хода
+# СДЕЛАТЬ: а почему начал пропадать ферзь, я уже отследить не успеваю — найдите сами, там что-то со ссылками в полях объекта хода
+print(game)
+
+game.redo()
+print('\nПовтор хода:')
+print(game)
+
+game.to_step(2)
+print('\nПереход на начало 2 хода:')
+print(game)
+
+print('\nЗапись шахматной партии:')
 print(game.game_log)
 
 
@@ -297,3 +322,8 @@ stdout
 5: WQh5f7
 
 """
+
+
+# КОММЕНТАРИЙ: вообще, молодец! не всё прям гладко, конечно, кое-где выбраны явно не лучшие решения, кое-где надо просто внимательнее быть — но цели упражнения, я считаю, выполнены
+
+# ИТОГ: так держать — 10/12
